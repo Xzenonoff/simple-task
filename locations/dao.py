3070abba7fff6bb746567c040a082fbbe7d83838
Xzenoff <xzenonoff@gmail.com>
2023-05-18 21:40:31 +0300

    task done

--- locations/dao.py
+++ locations/dao.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from typing import List
 from benchmark import timing
 
+
 @timing
 def get_current_dislocation() -> List:
     """
@@ -13,7 +14,8 @@ def get_current_dislocation() -> List:
     Для того, чтобы получить предсказанную дату прибытия, необходимо вызывать сервис 'get_predicted_dates'
     """
     locations = []
-    arrivale_dates = [None, None, None, datetime.now() - timedelta(days=3), datetime.now()]
+    arrivale_dates = [None, None, None, datetime.now() - timedelta(days=3),
+                      datetime.now()]
     time.sleep(2)
 
     for i in range(0, 20000):
@@ -21,10 +23,12 @@ def get_current_dislocation() -> List:
         location = {
             "wagon": random.randint(10000, 90000),
             "invoice": f"{random.randint(1, 30000)}__HASH__",
-            "arrivale_date": arrivale_date.strftime("%d.%m.%Y") if arrivale_date else None,            
+            "arrivale_date": arrivale_date.strftime(
+                "%d.%m.%Y") if arrivale_date else None,
         }
-        locations.append(location)    
+        locations.append(location)
     return locations
+
 
 @timing
 def get_predicted_date_by_invoices(invoices: List) -> List:
@@ -34,15 +38,32 @@ def get_predicted_date_by_invoices(invoices: List) -> List:
     """
     time.sleep(1)
     predicted_results = []
-    for invoice in invoices:        
+    for invoice in invoices:
         predicted_date = datetime.now() + timedelta(days=random.randint(1, 5))
         data = {
             "invoice": invoice,
             "predicted_date": predicted_date.strftime("%d.%m.%Y")
         }
         predicted_results.append(data)
-    
+
     return predicted_results
+
+
+def get_no_date_invoices_from_locations(locations):
+    invoices = [
+        location.get('invoice') for location in locations
+        if location.get('arrivale_date') is None
+    ]
+    return invoices
+
+
+def find_new_date_for_invoice(location, predicted_date):
+    for element in predicted_date:
+        if element.get('invoice') == location.get('invoice'):
+            new_date = element.get('predicted_date')
+            predicted_date.remove(element)
+            return new_date
+
 
 @timing
 def api_call():
@@ -52,19 +73,16 @@ def api_call():
     только по вагоном, у которых она отсутствует
     """
     locations = get_current_dislocation()
-    # Получить список уникальных накладных из текущей дислокации только по тем вагонам,
-    # где arrivale_date = None
-    invoices = []
-    predicted_data = get_predicted_date_by_invoices(invoices)
 
-    # Обновить оригинальный список вагонов данными, которые прислал сервис get_predicted_dates().
-    # Заменить вагоны, где arrivale_date = None на соответствующее поле predicted_date.    
+    invoices = get_no_date_invoices_from_locations(locations)
+    predicted_date = get_predicted_date_by_invoices(invoices)
 
-    
-
-
-
-    
+    for location in locations:
+        if location.get('arrivale_date') is None:
+            new_date = find_new_date_for_invoice(location, predicted_date)
+            location.update({'arrivale_date': new_date})
+    return locations
 
 
-
+if __name__ == '__main__':
+    api_call()
